dba56f3a9a408ca342c5691107a221b283d6d932
davidhewitt <davidhewitt@users.noreply.github.com>
2021-03-17 21:09:59 +0000

    Upload documentation for main

---
+++ main/doc/pyo3/proc_macro/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"attr":[["pyclass",""],["pyfunction","The proc macro attributes"],["pymethods",""],["pymodule","Internally, this proc macro create a new c function called `PyInit_{my_module}` that then calls the init function you provided"],["pyproto","The proc macro attributes"]]});
