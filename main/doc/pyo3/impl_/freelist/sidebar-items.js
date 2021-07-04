c7d6a417163a6a0a5bf00b27f8b71c62376148fd
davidhewitt <davidhewitt@users.noreply.github.com>
2021-07-04 15:46:53 +0000

    Upload documentation for main

--- main/doc/pyo3/impl_/freelist/sidebar-items.js
+++ main/doc/pyo3/impl_/freelist/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Slot","Represents a slot of a [`FreeList`]."]],"struct":[["FreeList",""]]});
+initSidebarItems({"enum":[["Slot","Represents a slot of a [`FreeList`]."]],"struct":[["FreeList","A free allocation list."]]});
