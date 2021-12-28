132bef772d26aaac70b177abdc3478ed92d531bd
messense <messense@users.noreply.github.com>
2021-12-28 11:15:46 +0000

    Upload documentation for main

--- main/doc/crates.js
+++ main/doc/crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["pyo3"];
+window.ALL_CRATES = ["pyo3","pyo3_build_config"];
---
+++ main/doc/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["pyo3_build_config"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"pyo3_build_config/struct.PythonVersion.html\" title=\"struct pyo3_build_config::PythonVersion\">PythonVersion</a>","synthetic":false,"types":["pyo3_build_config::impl_::PythonVersion"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
