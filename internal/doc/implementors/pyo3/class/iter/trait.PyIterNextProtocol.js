9dbb83e469099829c343412a792ab181d6b45ee2
davidhewitt <davidhewitt@users.noreply.github.com>
2022-01-27 20:19:29 +0000

    Upload internal documentation

--- internal/doc/implementors/pyo3/class/iter/trait.PyIterNextProtocol.js
+++ internal/doc/implementors/pyo3/class/iter/trait.PyIterNextProtocol.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["pyo3_pytests"] = [{"text":"impl&lt;'p&gt; PyIterNextProtocol&lt;'p&gt; for <a class=\"struct\" href=\"pyo3_pytests/pyclass_iter/struct.PyClassIter.html\" title=\"struct pyo3_pytests::pyclass_iter::PyClassIter\">PyClassIter</a>","synthetic":false,"types":["pyo3_pytests::pyclass_iter::PyClassIter"]}];
+implementors["pyo3_pytests"] = [{"text":"impl&lt;'p&gt; <a class=\"trait\" href=\"pyo3/class/iter/trait.PyIterNextProtocol.html\" title=\"trait pyo3::class::iter::PyIterNextProtocol\">PyIterNextProtocol</a>&lt;'p&gt; for <a class=\"struct\" href=\"pyo3_pytests/pyclass_iter/struct.PyClassIter.html\" title=\"struct pyo3_pytests::pyclass_iter::PyClassIter\">PyClassIter</a>","synthetic":false,"types":["pyo3_pytests::pyclass_iter::PyClassIter"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- internal/doc/pyo3/types/tuple/sidebar-items.js
+++ internal/doc/pyo3/types/tuple/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["wrong_tuple_length",""]],"macro":[["tuple_conversion",""]],"struct":[["PyTuple","Represents a Python `tuple` object."],["PyTupleIterator","Used by `PyTuple::iter()`."]]});
+initSidebarItems({"fn":[["new_from_iter",""],["wrong_tuple_length",""]],"macro":[["tuple_conversion",""]],"struct":[["PyTuple","Represents a Python `tuple` object."],["PyTupleIterator","Used by `PyTuple::iter()`."]]});
